945aa6943d4a0ca08c5ca1babd89cff9747e995d
m.sergeenkov <m.sergeenkov@int.robotbull.com>
2020-05-22 00:48:55 +0300

    Фикс доставания кода из query string

--- src/pages/Redirect.js
+++ src/pages/Redirect.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import queryString from 'query-string';
 
 export default function RedirectPage(props) {
-    console.log(props)
+    let params = queryString.parse(props.location.search);
+    console.log(params.code);
     return (
         <div className="redirect">
-            <p>{new URLSearchParams(props.location.search).get("code")}</p>
+            {/*<p>{new URLSearchParams(props.location.search).get("code")}</p>*/}
         </div>
     )
 }
